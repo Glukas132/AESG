10b24e6a5764cc593caf2546618aa4cfbbe3231a
Lukas <Lukas@DESKTOP>
2021-10-27 15:22:20 +0200

    plots und so

--- Exercise_1.m
+++ Exercise_1.m
@@ -6,14 +6,10 @@ else
     save("GO513138.TRO.mat", "stations");
     save("GO513138.data_all.mat", "data_all");
 end
-if exist("raob_soundings14969.txt.mat", "file")
-    load("raob_soundings14969.txt.mat");
-else
-    RS = read_RS_data("raob_soundings14969.txt");
-    save('raob_soundings14969.txt.mat', "RS");
-end
-ZHD = Saastamoinen_ZHD(1013.25, stations.lat, stations.H);
-ZTD = data_all.ZTD;
+[Nw_RS, H_RS, ZTD_RS, lat0, lon0, H0] = read_RS_data("raob_soundings14969.txt");
+%ZHD = Saastamoinen_ZHD(1013.25, stations.lat, stations.H);
+ZTD = [data_all.ZTD];
+ZTD = ZTD(1:24:end);
 %%
 figure
 geoscatter(stations.lat, stations.lon);
@@ -29,3 +25,31 @@ figure
 geoscatter(stations.lat, stations.lon, [], ZTD, 'filled');
 colorbar;
 title('ZTD values')
+%%
+figure
+subplot(2,1,1)
+scatter([1:254], stations.H,'.')
+subplot(2,1,2)
+scatter([1:254], ZTD,'.')
+%%
+temp = [data_all.ZTD];
+figure
+scatter([1:24],temp(1:24))
+%%
+figure
+plot(Nw_RS, H_RS)
+%%
+len = zeros(1,254);
+C = zeros(255,3);
+C(:,3) = 1;
+for i = 1:254
+    len(i) = distance(lat0,lon0, stations.lat(i), stations.lon(i));
+end
+[m, idx] = min(len);
+lats = [lat0, stations.lat];
+lons = [lon0, stations.lon];
+C(1,:) = [1 0 0]; % RS station: Rot
+C(idx+1,:) = [0 0 0]; % nächste GNSS station: schwarz
+figure
+gx = geoaxes('Basemap','colorterrain');
+geoscatter(gx, lats, lons, [], C)
